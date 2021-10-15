79f6743362454b19882c800795890f811ca43d8d
pgl <1801688332@qq.com>
2021-10-15 15:02:59 +0800

    feat: 配置路由

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,17 @@
 const Koa = require('koa');
 
+const Router = require('koa-router');
+
 const app = new Koa();
 
-module.exports = app;
+const userRouter = new Router({ prefix: '/users' });
 
+userRouter.post('/', (ctx, next) => {
+  ctx.body = '创建用户成功～';
+});
+
+// 进行调用
+app.use(userRouter.routes());
+app.use(userRouter.allowedMethods());
+
+module.exports = app;
