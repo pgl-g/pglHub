daa0ebc7bd297d532f71c77a5324b760b3b01743
pgl <1801688332@qq.com>
2021-10-15 15:30:45 +0800

    fix: 调整环境变量

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 const app = require('./app');
+// TDOO：先写死这边有问题
 const config = require('./app/config');
 
 // 启动服务
-app.listen(config.APP_PORT, () => {
+app.listen(8888, () => {
   console.log('服务器启动成功~');
 });
 
