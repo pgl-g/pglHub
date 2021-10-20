689f10f83dc042f2cdbabfbb765f18dd90bd5d95
pgl <1801688332@qq.com>
2021-10-20 22:22:07 +0800

    fix:封装route，减少耦合

--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,13 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
-
-const userRouter = require('../router/user.router');
-const authRouter = require('../router/auth.router');
 const errorHandler = require('./error-handle');
+const useRoutes = require('../router');
 
 const app = new Koa();
 
 // 进行调用
 app.use(bodyParser());
-app.use(userRouter.routes());
-app.use(userRouter.allowedMethods());
-app.use(authRouter.routes());
-app.use(authRouter.allowedMethods());
-
+useRoutes(app);
 app.on('error', errorHandler);
 
 module.exports = app;
